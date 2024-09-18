e696929d7caf7cd8b4c46301a869cb4339992f02
Wilmar Andres Rojas Villafañe <106172929+wirovi07@users.noreply.github.com>
2024-09-18 15:27:14 -0500

    Vista e Importacion de FullCalendar

---
+++ resources/js/Components/Calendar.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <FullCalendar :options="calendarOptions"></FullCalendar>
+    </div>
+</template>
+
+<script>
+import FullCalendar from '@fullcalendar/vue3'
+import daygrid from '@fullcalendar/daygrid';
+import interactionPlugin from '@fullcalendar/interaction';
+import timeGrip from '@fullcalendar/timegrid'
+
+export default {
+    name: 'Calendar',
+    components: {
+        FullCalendar 
+    },
+    data() {
+        return {
+            calendarOptions: {
+                plugins: [daygrid, interactionPlugin, timeGrip],
+                initialView: 'dayGridMonth',
+                headerToolbar: {
+                    left: 'prev, next, today',
+                    center: 'title',
+                    right: 'dayGridMonth,timeGridWeek,timeGridDay'
+                }
+            }
+        }
+    }
+}
+</script>
---
+++ resources/js/Pages/Agenda/Books.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h2>Calendario</h2>
+        <Calendar></Calendar>
+    </div>
+</template>
+
+<script>
+    import Calendar from '../../Components/Calendar.vue'
+
+    export default {
+        name: 'Books',
+        components: {
+            Calendar
+        }
+    }
+</script>
